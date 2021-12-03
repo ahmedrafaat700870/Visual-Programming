b30230d5d804766cae0c126ed386466d20cbc823
ahmedrafaat700870 <71382395+ahmedrafaat700870@users.noreply.github.com>
2021-12-02 19:35:02 -0800

    Add files via upload

---
+++ Task 3 7/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿using System;
+
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Year = new System.Windows.Forms.TextBox();
+            this.Month = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Week = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Resalut = new System.Windows.Forms.Label();
+            this.Menit = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Hour = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Day = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(170, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter the Year";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Year
+            // 
+            this.Year.Location = new System.Drawing.Point(531, 23);
+            this.Year.Name = "Year";
+            this.Year.Size = new System.Drawing.Size(100, 20);
+            this.Year.TabIndex = 1;
+            this.Year.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.Year.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Year_KeyUp);
+            // 
+            // Month
+            // 
+            this.Month.Location = new System.Drawing.Point(531, 82);
+            this.Month.Name = "Month";
+            this.Month.Size = new System.Drawing.Size(100, 20);
+            this.Month.TabIndex = 3;
+            this.Month.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Month_KeyUp);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(170, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Enter the Month";
+            // 
+            // Week
+            // 
+            this.Week.Location = new System.Drawing.Point(531, 147);
+            this.Week.Name = "Week";
+            this.Week.Size = new System.Drawing.Size(100, 20);
+            this.Week.TabIndex = 5;
+            this.Week.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Week_KeyUp);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(170, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Enter the Week";
+            // 
+            // Resalut
+            // 
+            this.Resalut.AutoSize = true;
+            this.Resalut.Location = new System.Drawing.Point(363, 411);
+            this.Resalut.Name = "Resalut";
+            this.Resalut.Size = new System.Drawing.Size(0, 13);
+            this.Resalut.TabIndex = 6;
+            // 
+            // Menit
+            // 
+            this.Menit.Location = new System.Drawing.Point(531, 329);
+            this.Menit.Name = "Menit";
+            this.Menit.Size = new System.Drawing.Size(100, 20);
+            this.Menit.TabIndex = 12;
+            this.Menit.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Menit_KeyUp);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(170, 336);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Enter the Menit";
+            // 
+            // Hour
+            // 
+            this.Hour.Location = new System.Drawing.Point(531, 264);
+            this.Hour.Name = "Hour";
+            this.Hour.Size = new System.Drawing.Size(100, 20);
+            this.Hour.TabIndex = 10;
+            this.Hour.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Hour_KeyUp);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(170, 271);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(74, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Enter the hour";
+            // 
+            // Day
+            // 
+            this.Day.Location = new System.Drawing.Point(531, 201);
+            this.Day.Name = "Day";
+            this.Day.Size = new System.Drawing.Size(100, 20);
+            this.Day.TabIndex = 8;
+            this.Day.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Day_KeyUp);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(170, 208);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(72, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Enter the Day";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Menit);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Hour);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Day);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.Resalut);
+            this.Controls.Add(this.Week);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Month);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Year);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Year;
+        private System.Windows.Forms.TextBox Month;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Week;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Resalut;
+        private System.Windows.Forms.TextBox Menit;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox Hour;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox Day;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Task 3 7/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int calc_Year = 0;
+        int calc_Month = 0;
+        int calc_Week = 0;
+        int calc_Day = 0;
+        int calc_Hour = 0;
+        int calc_Menit = 0;
+        int Sumtion (int a , int b , int c , int d , int e , int f)
+        {
+            return a + b + c + d + e + f;
+        }
+        private void Month_KeyUp(object sender, KeyEventArgs e)
+        {
+            string[] month = new string[]
+            {
+                "Month",
+                Month.Text
+            };
+            calc_Month = calc(month);
+            Resalut.Text = Convert.ToString(Sumtion(calc_Year, calc_Month, calc_Week, calc_Day, calc_Hour, calc_Menit)) + " Seconds";
+        }
+        private void Year_KeyUp(object sender, KeyEventArgs e)
+        {
+            string[] year = new string[] { "Year", Year.Text };
+            calc_Year = calc(year);
+            Resalut.Text = Convert.ToString(Sumtion(calc_Year, calc_Month, calc_Week, calc_Day, calc_Hour, calc_Menit)) + " Seconds";
+        }
+        private void Week_KeyUp(object sender, KeyEventArgs e)
+        {
+            string[] week = new string[]
+            {
+                "Week",
+                Week.Text
+            };
+            calc_Week = calc(week);
+            Resalut.Text = Convert.ToString(Sumtion(calc_Year, calc_Month, calc_Week, calc_Day, calc_Hour, calc_Menit)) + " Seconds";
+        }
+        private void Day_KeyUp(object sender, KeyEventArgs e)
+        {
+            string[] day = new string[]
+         {
+                "Day" ,
+                Day.Text
+         };
+            calc_Day = calc(day);
+            Resalut.Text = Convert.ToString(Sumtion(calc_Year, calc_Month, calc_Week, calc_Day, calc_Hour, calc_Menit)) + " Seconds";
+        }
+        private void Hour_KeyUp(object sender, KeyEventArgs e)
+        {
+            string[] hour = new string[]
+            {
+                "Hour" ,
+                Hour.Text
+            };
+            calc_Hour = calc(hour);
+            Resalut.Text = Convert.ToString(Sumtion(calc_Year, calc_Month, calc_Week, calc_Day, calc_Hour, calc_Menit))+ " Seconds";
+        }
+        private void Menit_KeyUp(object sender, KeyEventArgs e)
+        {
+            string[] menit = new string[]
+            {
+                "Menit",
+                Menit.Text
+            };
+            calc_Menit = calc(menit);
+            Resalut.Text = Convert.ToString(Sumtion(calc_Year, calc_Month, calc_Week, calc_Day, calc_Hour, calc_Menit)) + " Seconds";
+        }
+        int calc(string[] info)
+        {
+            int a = 0;
+            try
+            {
+                if (info[0] == "Year")
+                {
+                    a = Convert.ToInt32(info[1]) * 365 * 24 * 60 * 60;
+                }
+                else if (info[0] == "Month")
+                {
+                    a = Convert.ToInt32(info[1]) * 30 * 24 * 60 * 60;
+                }
+                else if (info[0] == "Week")
+                {
+                    a = Convert.ToInt32(info[1]) * 7 * 24 * 60 * 60;
+                }
+                else if (info[0] == "Day")
+                {
+                    a = Convert.ToInt32(info[1]) * 24 * 60 * 60;
+                }
+                else if (info[0] == "Hour")
+                {
+                    a = Convert.ToInt32(info[1]) * 60 * 60;
+                }
+                else if (info[0] == "Menit")
+                {
+                    a = Convert.ToInt32(info[1]) * 60;
+                }
+            }
+            catch (NotImplementedException e)
+            {
+                Console.WriteLine(e);
+            }
+            catch (System.FormatException e)
+            {
+                Console.WriteLine(e);
+            }
+            return a;
+        }   
+    }
+}
