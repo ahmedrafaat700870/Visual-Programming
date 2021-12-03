498db48e4b79734e8e1a17be89a10a1d39ad00a7
ahmedrafaat700870 <71382395+ahmedrafaat700870@users.noreply.github.com>
2021-12-02 19:56:45 -0800

    Add files via upload

---
+++ Task4_3/Task4_3/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Task4_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.show = new System.Windows.Forms.Button();
+            this.birthDate = new System.Windows.Forms.TextBox();
+            this.Age = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter your Birth Date";
+            // 
+            // show
+            // 
+            this.show.Location = new System.Drawing.Point(280, 137);
+            this.show.Name = "show";
+            this.show.Size = new System.Drawing.Size(75, 23);
+            this.show.TabIndex = 1;
+            this.show.Text = "Show Age";
+            this.show.UseVisualStyleBackColor = true;
+            this.show.Click += new System.EventHandler(this.show_Click);
+            // 
+            // birthDate
+            // 
+            this.birthDate.Location = new System.Drawing.Point(419, 43);
+            this.birthDate.Name = "birthDate";
+            this.birthDate.Size = new System.Drawing.Size(100, 20);
+            this.birthDate.TabIndex = 2;
+            // 
+            // Age
+            // 
+            this.Age.AutoSize = true;
+            this.Age.Location = new System.Drawing.Point(310, 217);
+            this.Age.Name = "Age";
+            this.Age.Size = new System.Drawing.Size(0, 13);
+            this.Age.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(372, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(194, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Date shode be like this year/month/day";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 289);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Age);
+            this.Controls.Add(this.birthDate);
+            this.Controls.Add(this.show);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button show;
+        private System.Windows.Forms.TextBox birthDate;
+        private System.Windows.Forms.Label Age;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Task4_3/Task4_3/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task4_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void show_Click(object sender, EventArgs e)
+        {
+            string tex = birthDate.Text;
+            string[] date_Now = new string[]
+            {
+                DateTime.Now.Year.ToString(),
+                DateTime.Now.Month.ToString(),
+                DateTime.Now.Day.ToString()
+            };
+            string[] birth_Date = Detial_Date(tex);
+            string[] age = clac_Age(birth_Date, date_Now);
+            Age.Text = "Your Age is : " +age[0] + " Year  and " + age[1] + " Month and "+ age[2] +" Day";
+        }
+        string [] Detial_Date(string date) // function retrun array contian {year , month , days} from stirng year/month/day.
+        {
+            string[] Birth_Date ;
+            int count = 0;
+            string year = "";
+            string mon = "";
+            string day = "";
+            for (int i = 0;i< date.Length; i++)
+            {
+                if (count == 0)
+                {
+                    if (date[i] == Convert.ToChar("/"))
+                    {
+                        count++;
+                    } else
+                    {
+                        year += Convert.ToString(date[i]);
+                    }
+                } else if (count == 1) {
+                    if (date[i] == Convert.ToChar("/"))
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        mon += Convert.ToString(date[i]);
+                    }
+                } else
+                {
+                    day += Convert.ToString(date[i]);
+                }
+            }
+            Birth_Date = new string[3]
+            {
+                year, mon, day
+            };
+            return Birth_Date ;
+        }
+        string [] clac_Age (string [] birth , string [] date_now) // function calc Age .
+        {
+            int[] new_birth = converttointarry(birth);
+            int[] new_Date_Now = converttointarry(date_now);
+            int[] age = new int[birth.Length];
+            string []new_age = new string [age.Length];
+            if (new_birth[0] < new_Date_Now[0])
+            {
+                age[0] = new_Date_Now[0] - new_birth[0];
+            } else
+            {
+                new_birth[0] += 30;
+                new_birth[1] -= 1;
+                age[0] = new_Date_Now[0] - new_birth[0];
+            }
+            if (new_birth[1] < new_Date_Now[1])
+            {
+                age[1] = new_Date_Now[1] - new_birth[1];
+            }
+            else
+            {
+                new_birth[1] += 12;
+                new_birth[2] -= 1;
+                age[1] = new_Date_Now[1] - new_birth[1];
+            }
+            age[2] = new_Date_Now[2] - new_birth[2];
+            for (int i = 0; i < age.Length; i++)
+            {
+                new_age[i] = Convert.ToString(age[i]);
+            }
+            return new_age;
+        }
+        int [] converttointarry (string[] s) //function return int arr from string arr .
+        {
+            int[] n = new int[s.Length];
+            for (int i = 0; i< s.Length; i++ )
+            {
+                n[i] = Convert.ToInt32(s[i]);
+            }
+            return n;
+        }
+     }
+}
