80de880a1fae3789a2a8477635b8de1c62f99b2f
ahmedrafaat700870 <71382395+ahmedrafaat700870@users.noreply.github.com>
2021-12-02 19:57:07 -0800

    Add files via upload

---
+++ Task4_4/Task4_4/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task4_4
+{
+    
+    public partial class Form1 : Form
+    {
+        char opr;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            opr = '*';
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            opr = '/';
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            opr = '+';
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            opr = '-';
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int first = Convert.ToInt32(firstNum.Text);
+            int second = Convert.ToInt32(secondNum.Text);
+            int resalt = res(first, second,opr);
+            show.Text = firstNum.Text+" " + Convert.ToString(opr)+" " + secondNum.Text +" = " +Convert.ToString(resalt) ;
+        }
+        int res(int fir , int last , char op)
+        {
+            if (op == '+')
+            {
+                return fir + last;
+            } else if (op == '-')
+            {
+                return fir - last;
+            } else if (op == '/')
+            {
+                return fir / last;
+            } else if (op == '*')
+            {
+                return fir * last;
+            } else
+            {
+                return 0;
+            }
+        }
+    }
+}
