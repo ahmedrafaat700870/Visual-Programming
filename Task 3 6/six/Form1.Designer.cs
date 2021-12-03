c31fca5863790cf209388c2d348b20d582270268
ahmedrafaat700870 <71382395+ahmedrafaat700870@users.noreply.github.com>
2021-12-02 19:34:19 -0800

    Add files via upload

---
+++ Task 3 6/six/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace six
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.length = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn = new System.Windows.Forms.Button();
+            this.show = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // length
+            // 
+            this.length.AutoSize = true;
+            this.length.Location = new System.Drawing.Point(60, 55);
+            this.length.Name = "length";
+            this.length.Size = new System.Drawing.Size(119, 13);
+            this.length.TabIndex = 0;
+            this.length.Text = "Enter the Length fo Mat";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(216, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(251, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(534, 50);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(139, 23);
+            this.btn.TabIndex = 2;
+            this.btn.Text = "show";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // show
+            // 
+            this.show.AutoSize = true;
+            this.show.Location = new System.Drawing.Point(12, 129);
+            this.show.Name = "show";
+            this.show.Size = new System.Drawing.Size(0, 13);
+            this.show.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(60, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Tag = "";
+            this.label1.Text = "Dont Enter more than 200";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(738, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.show);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.length);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label length;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.Label show;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Task 3 6/six/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace six
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String lenght = textBox1.Text;
+            String f = creatArr(lenght, check(lenght, 200));
+            if (f == "Error")
+            {
+                MessageBox.Show("You Can Not Enter mor than 200");
+            }
+            else
+            {
+                show.Text = f;
+            }
+        }
+        bool check(string n , int length)
+        {
+            if (Convert.ToInt32(n) > length || n.Equals(null) || n.Equals(""))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+           
+        }
+
+        String creatArr(string length , bool done)
+        {
+            Random n = new Random();
+            int a;
+            String m = "";
+            if (done)
+            {
+                String[] arr = new string[Convert.ToInt16(length)];
+                for (int i = 0; i < Convert.ToInt16(length); i++)
+                {
+                    a = n.Next(20);
+                    if (i % 45 != 0 || i == 0)
+                    {
+                        m = m + "  " + Convert.ToString(a);
+                    }
+                    else
+                    {
+                        m = m + "\n" + Convert.ToString(a);
+                    }
+                }
+                return m;
+            }
+            else
+            {
+                return "Error";
+            }
+          
+        }
+    }
+}
