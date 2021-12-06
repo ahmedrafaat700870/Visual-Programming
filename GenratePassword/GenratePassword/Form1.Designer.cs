227adb565aaeb0b4de1e32d9c5d161102a41610f
ahmedrafaat700870 <71382395+ahmedrafaat700870@users.noreply.github.com>
2021-12-05 20:13:41 -0800

    Add files via upload

---
+++ GenratePassword/GenratePassword/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace GenratePassword
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.LowerCase = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(169, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(213, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(229, 195);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start Genrate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(28, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(543, 69);
+            this.panel1.TabIndex = 3;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(440, 24);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(95, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Contian Letter ";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter the size of PassWord";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.LowerCase);
+            this.panel2.Controls.Add(this.radioButton2);
+            this.panel2.Enabled = false;
+            this.panel2.Location = new System.Drawing.Point(178, 105);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(311, 69);
+            this.panel2.TabIndex = 4;
+            // 
+            // LowerCase
+            // 
+            this.LowerCase.AutoSize = true;
+            this.LowerCase.Location = new System.Drawing.Point(27, 27);
+            this.LowerCase.Name = "LowerCase";
+            this.LowerCase.Size = new System.Drawing.Size(81, 17);
+            this.LowerCase.TabIndex = 3;
+            this.LowerCase.TabStop = true;
+            this.LowerCase.Text = "Lower Case";
+            this.LowerCase.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(201, 27);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(81, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Upper Case";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Location = new System.Drawing.Point(28, 259);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(724, 69);
+            this.panel3.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Green;
+            this.label2.Location = new System.Drawing.Point(198, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 25);
+            this.label2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RadioButton LowerCase;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ GenratePassword/GenratePassword/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GenratePassword
+{
+    public partial class Form1 : Form
+    {
+        static int Size;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Size = Convert.ToInt32(textBox1.Text);
+            if (checkBox1.Checked)
+            {
+                // leter and number .
+                
+                if (LowerCase.Checked)
+                {
+                    // Letter Lower Case and Numbers .
+                    label2.Text = GenrateThePassWord(Size, "No", "Yes",Charcter);
+                } else
+                {
+                    // Leter upper Case and Numbers .
+                    label2.Text = GenrateThePassWord(Size, "Yes", "Yes", Charcter);
+                }
+            } else
+            {
+                // genrate Number .
+                label2.Text = GenrateThePassWord(Size, "No", "No", Charcter);
+            }
+        }
+        public static Random Charcter = new Random();
+        public static string GenrateThePassWord(int Size,string upperCase ,string Letter,Random num)
+        {
+            string[] pass = new string[Size];
+            string lastPass = "";
+            int b = num.Next(0, 3);
+            if (Letter.Equals("No"))
+            {
+                // genrate Number.
+                for (int i = 0; i < pass.Length; i++)
+                {
+                    
+                    pass[i] = Convert.ToString(getrateNum(Charcter));
+                }
+            } else if (upperCase.Equals("No"))
+            { // genrate Numbers And Lerter LowerCase .
+                for (int i = 0; i < Size; i++)
+                {
+                    if (b == 0)
+                    {
+                        //genrate Number .
+                        pass[i] = Convert.ToString(getrateNum(Charcter));
+                        b = num.Next(0, 2);
+                    }
+                    else
+                    {
+                        //genrate Char .
+                        pass[i] = Convert.ToString(Charcters(Charcter));
+                        b = num.Next(0, 2);
+                    }
+                }
+            } else
+            { // genrate Number and Letter upperCase .
+                for (int i = 0; i < Size; i++)
+                {
+                    if (b == 0)
+                    {
+                        //genrate Number .
+                        pass[i] = Convert.ToString(getrateNum(Charcter));
+                        b = num.Next(0, 2);
+                    }
+                    else
+                    {
+                        //genrate Char .
+                        pass[i] = Convert.ToString(Charcters(Charcter)).ToUpper();
+                        b = num.Next(0, 2);
+                    }
+                }
+            }
+            for (int i = 0; i< pass.Length; i++)
+            {
+                lastPass += pass[i];
+            }
+            return lastPass;
+            
+        }
+        public static int getrateNum(Random num)
+        {
+            int Number = num.Next(0, 10);
+            return Number;
+        }
+        public static char Charcters (Random c)
+        {
+            char charcter = (char) ('a' + c.Next(0, 26));
+            return charcter;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            panel2.Enabled = true;   
+        }
+    }
+}
