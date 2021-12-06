1360820aa71ca4ebf459b1d63f76d83b3c31ca1b
ahmedrafaat700870 <71382395+ahmedrafaat700870@users.noreply.github.com>
2021-12-05 20:14:51 -0800

    Add files via upload

---
+++ Rock Baber Secsoir/Task5.1/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Task5._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Rock = new System.Windows.Forms.Button();
+            this.Para = new System.Windows.Forms.Button();
+            this.Baber = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Rock
+            // 
+            this.Rock.Location = new System.Drawing.Point(36, 34);
+            this.Rock.Name = "Rock";
+            this.Rock.Size = new System.Drawing.Size(75, 23);
+            this.Rock.TabIndex = 0;
+            this.Rock.Text = "Rock";
+            this.Rock.UseVisualStyleBackColor = true;
+            // 
+            // Para
+            // 
+            this.Para.Location = new System.Drawing.Point(252, 34);
+            this.Para.Name = "Para";
+            this.Para.Size = new System.Drawing.Size(75, 23);
+            this.Para.TabIndex = 1;
+            this.Para.Text = "Scissors";
+            this.Para.UseVisualStyleBackColor = true;
+            // 
+            // Baber
+            // 
+            this.Baber.Location = new System.Drawing.Point(477, 34);
+            this.Baber.Name = "Baber";
+            this.Baber.Size = new System.Drawing.Size(75, 23);
+            this.Baber.TabIndex = 2;
+            this.Baber.Text = "Baber";
+            this.Baber.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(62, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(87, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(260, 40);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(86, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.Rock);
+            this.panel1.Controls.Add(this.Para);
+            this.panel1.Controls.Add(this.Baber);
+            this.panel1.Enabled = false;
+            this.panel1.Location = new System.Drawing.Point(105, 155);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(598, 90);
+            this.panel1.TabIndex = 5;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Enabled = false;
+            this.panel2.Location = new System.Drawing.Point(175, 280);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(434, 88);
+            this.panel2.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(255, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Socre Player Tow";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Soore Player One";
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.checkBox2);
+            this.panel3.Controls.Add(this.checkBox1);
+            this.panel3.Controls.Add(this.button4);
+            this.panel3.Location = new System.Drawing.Point(218, 21);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(359, 100);
+            this.panel3.TabIndex = 7;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(26, 58);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(119, 17);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "Play With Computer";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(26, 16);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(97, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Play With Frind";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(219, 37);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Start Game ";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 402);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Rock;
+        private System.Windows.Forms.Button Para;
+        private System.Windows.Forms.Button Baber;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Rock Baber Secsoir/Task5.1/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task5._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static int How_Play = 0;
+        static int Score_Player_One = 0;
+        static int Score_Player_Tow = 0;
+        static string player_one = "";
+        static string player_two = "";
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            panel2.Enabled = true;
+            panel1.Enabled = true;
+            if (checkBox1.Checked)
+            {
+                this.Rock.Click += (s, ev) =>  Button_Click("rock", "", "Palyer One Win", "Player Tow Win", "Equal");
+                this.Baber.Click += (s, ev) => Button_Click("baber", "", "Palyer One Win", "Player Tow Win", "Equal");
+                this.Para.Click += (s, ev) => Button_Click("para", "", "Palyer One Win", "Player Tow Win", "Equal");
+            } if (checkBox2.Checked)
+            {
+                this.Rock.Click += (s, ev) => Button_Click("rock", "com", "Palyer One Win", "Computer  Win", "Equal");
+                this.Baber.Click += (s, ev) => Button_Click("baber", "com", "Palyer One Win", "Computer Win", "Equal");
+                this.Para.Click += (s, ev) => Button_Click("para", "com", "Palyer One Win", "Computer Win", "Equal");
+                // paly with Computer .
+            }
+        }
+         private void Button_Click(string choice ,string frindOrCom , string pla_one_win , string pal_tow_win , string equal)
+         {
+            play(choice, frindOrCom);
+            playAgain(how_win(player_one, player_two), pla_one_win, pal_tow_win, equal);
+            textBox1.Text = Convert.ToString(Score_Player_One);
+            textBox2.Text = Convert.ToString(Score_Player_Tow);
+         }
+       
+        public void play(string str , string p)
+        {
+            if (How_Play == 0 && !p.Equals("com"))
+            {
+                player_one = str;
+                Form1.How_Play++;
+            }
+            else if (How_Play == 0 && p.Equals("com"))
+            {
+                // play with Computer .
+                if (Computer_Play().Equals(0))
+                {
+                    player_one = str;
+                    player_two = "rock";
+                }
+                else if (Computer_Play().Equals(1))
+                {
+                    player_one = str;
+                    player_two = "para";
+                }
+                else if (Computer_Play().Equals(2))
+                {
+                    player_one = str;
+                    player_two = "baber";
+                }
+            }
+            else 
+            {
+                // play with Player .
+                player_two = str;
+                Form1.How_Play--;
+            }
+        }
+        public int how_win (string playerOne , string playerTow)
+        {
+            if (playerOne.Equals("rock"))
+            {
+                if (playerTow.Equals("para"))
+                {
+                    return 0;
+                }
+                else if (playerTow.Equals("baber"))
+                {
+                    return 1;
+                }
+                else if (playerTow.Equals("rock")) 
+                {
+                    return 2;
+                } else
+                {
+                    return 3;
+                }
+            } 
+            else if (playerOne.Equals("para"))
+            {
+                if (playerTow.Equals("rock"))
+                {
+                    return 1;
+                } else if (playerTow.Equals("baber"))
+                {
+                    return 0;
+                }
+                else if (playerTow.Equals("para"))
+                {
+                    return 2;
+                } else
+                {
+                    return 3;
+                }
+            } 
+            else if (playerOne.Equals("baber"))
+            {
+                if (playerTow.Equals("rock"))
+                {
+                    return 0;
+                }
+                else if (playerTow.Equals("para"))
+                {
+                    return 1;
+                }
+                else if (playerTow.Equals("baber")) 
+                {
+                    return 2;
+                }  else
+                {
+                    return 3;
+                }
+            }
+            else
+            {
+                return 3; // palyer one and palyer tow play the same play . 
+            }
+        }
+        public void playAgain (int n , string m1 ,string m2 , string m3)
+        {
+            DialogResult r;
+            if (n == 0 ) // playre one how win .
+            {
+                Score_Player_One++;
+                r = MessageBox.Show(m1 +" if you want to paly again press yes", "The Winner", MessageBoxButtons.YesNo);
+                if (r == DialogResult.Yes)
+                {
+                    player_one = "";
+                    player_two = "";
+                } else
+                {
+                    MessageBox.Show(Winer(Score_Player_One,Score_Player_Tow,m1 ,m2, m3));
+                    Score_Player_One = 0;
+                    Score_Player_Tow = 0;
+                    player_one = "";
+                    player_two = "";
+                } 
+            } else if (n == 1)
+            {
+                Score_Player_Tow++;
+                r = MessageBox.Show(m2+" if you wnat to play again press yes ", "the Winner", MessageBoxButtons.YesNo);
+                if (r == DialogResult.Yes)
+                {
+                    player_one = "";
+                    player_two = "";
+
+                } else
+                {
+                    MessageBox.Show(Winer(Score_Player_One, Score_Player_Tow, m1, m2, m3));
+                    Score_Player_One = 0;
+                    Score_Player_Tow = 0;
+                    player_one = "";
+                    player_two = "";
+                }
+            } else if (n == 2)
+            {
+                r = MessageBox.Show(m3 + " if you wnat to play anain press Yes", "Equal", MessageBoxButtons.YesNo);
+                if (r == DialogResult.Yes)
+                {
+                    player_one = "";
+                    player_two = "";
+                }
+                else
+                {
+                    MessageBox.Show(Winer(Score_Player_One, Score_Player_Tow, m1, m2, m3));
+                    Score_Player_One = 0;
+                    Score_Player_Tow = 0;
+                    player_one = "";
+                    player_two = "";
+                }
+            }
+        }
+        public string Winer(int score_one, int Score_tow ,string str,string str2 ,string str3)
+        {
+            if (score_one > Score_tow)
+            {
+                return str;
+            } else if (score_one < Score_tow)
+            {
+                return str2;
+            } else
+            {
+                return str3;
+            }
+
+        }
+        public int Computer_Play ()
+        {
+            Random n = new Random();
+            return n.Next(0, 3);
+        }
+    }
+}
